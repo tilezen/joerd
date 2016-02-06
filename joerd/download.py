9c93fed833abcfff2254040dfa25bdb32551feeb
Matt Amos <zerebubuth@gmail.com>
2016-02-06 02:34:15 +0000

    More sophisticated downloader which supports resuming broken or interrupted downloads.

--- joerd/download.py
+++ joerd/download.py
@@ -1,17 +1,108 @@
 from contextlib import contextmanager, closing
-import requests
+import urllib2
 import tempfile
 import os
+import logging
+import shutil
 
 
 @contextmanager
 def get(url, options={}):
+    """
+    Download a file to a temporary directory, returning it.
+
+    The options provided will control the behaviour of the download algorithm.
+
+      * 'tries' - The maximum number of tries to download the file before
+        giving up and raising an exception.
+      * 'timeout' - Timeout in seconds before considering the connection to
+        have failed.
+      * 'verifier' - A function which is called with a filelike object. It
+        should return True if the file is okay and appears to be fully
+        downloaded.
+    """
     with closing(tempfile.NamedTemporaryFile()) as tmp:
-        with closing(requests.get(url, stream=True)) as req:
-            for chunk in req.iter_content(chunk_size=10240):
-                if chunk:
-                    tmp.write(chunk)
-        tmp.flush()
+        # current file position = number of bytes read
+        filepos = 0
+
+        # file size when downloaded, if known
+        filesize = None
+
+        # number of attempts so far
+        tries = 0
+
+        # maximum number of attempts to make
+        max_tries = options.get('tries', 1)
+
+        # timeout for blocking operations (e.g: connect) in seconds
+        timeout = options.get('timeout', 60)
+
+        # verifier function
+        verifier = options.get('verifier')
+
+        # whether the server supports Range headers (if it doesn't we'll have
+        # to restart from the beginning every time).
+        accept_range = False
+
+        # we need to download _something_ if the file position is less than the
+        # known size, or the size is unknown.
+        while filesize is None or filepos < filesize:
+            req = urllib2.Request(url)
+
+            # if the server supports accept range, and we have a partial
+            # download then attemp to resume it.
+            if accept_range and filepos > 0:
+                assert filesize is not None
+                req.headers['Range'] = 'bytes=%s-%s' % (filepos, filesize - 1)
+            else:
+                # otherwise, truncate the file in readiness to download from
+                # scratch.
+                filepos = 0
+                tmp.seek(0, os.SEEK_SET)
+                tmp.truncate(0)
+
+            f = urllib2.urlopen(req, timeout=timeout)
+
+            # try to get the filesize, if the server reports it.
+            if filesize is None:
+                filesize = int(f.info().get('Content-Length'))
+
+            # detect whether the server accepts Range requests.
+            accept_range = f.info().get('Accept-Ranges') == 'bytes'
+
+            # copy data from the server
+            shutil.copyfileobj(f, tmp)
+
+            # update number of bytes read (this would be nicer if copyfileobj
+            # returned it.
+            filepos = tmp.tell()
+            tries += 1
+
+            # if we don't know how large the file is supposed to be, then
+            # verify it every time.
+            if filesize is None and verifier is not None:
+                # reset tmp file to beginning for verification
+                tmp.seek(0, os.SEEK_SET)
+                if verifier(tmp):
+                    break
+                # no need to reset here - since filesize is none, then we'll be
+                # downloading from scratch, which will truncate the file.
+
+            # explode if we've exceeded the number of allowed attempts
+            if tries > max_tries:
+                break
+
+        if tries > max_tries:
+            raise Exception("Max tries exceeded (%d) while downloading file %r"
+                            % (max_tries, url))
+
+        # verify the file, if it hasn't been verified before
+        if filesize is not None and verifier is not None:
+            # reset tmp file to beginning for verification
+            tmp.seek(0, os.SEEK_SET)
+            if not verifier(tmp):
+                raise Exception("File downloaded from %r failed verification"
+                                % url)
 
         tmp.seek(0, os.SEEK_SET)
-        yield tmp.file
+        yield tmp
--- tests/test_download.py
+++ tests/test_download.py
@@ -8,8 +8,12 @@ except ImportError:
     from http import server as http
 import contextlib
 from httptestserver import Server
+import re
+import sys
 
 
+# simple handler which does what most HTTP servers (should) do; responds with
+# the whole requested file.
 class _SimpleHandler(http.BaseHTTPRequestHandler):
     def __init__(self, value, *args):
         self.value = value
@@ -20,6 +24,58 @@ class _SimpleHandler(http.BaseHTTPRequestHandler):
         self.send_header('Content-Length', len(self.value))
         self.end_headers()
         self.wfile.write(self.value)
+
+
+# handler which emulates the GMTED server / TCP-layer rate-limiter; it drops
+# connections after some number of bytes.
+class _DroppingHandler(http.BaseHTTPRequestHandler):
+    def __init__(self, value, max_len, *args):
+        self.value = value
+        self.max_len = max_len
+        http.BaseHTTPRequestHandler.__init__(self, *args)
+
+    def _parse_range(self, r):
+        if r is None:
+            return None
+
+        m = re.match('bytes=([0-9]+)-([0-9]*)', r)
+        if not m:
+            return None
+
+        start = int(m.group(1))
+        end = int(m.group(2)) if len(m.group(2)) > 0 else None
+
+        if end is None:
+            end = min(start + self.max_len, len(self.value))
+        else:
+            end = min(end, start + self.max_len, len(self.value))
+
+        return (start, end)
+
+    def do_GET(self):
+        byte_range = self._parse_range(self.headers.get('Range'))
+
+        if byte_range is None:
+            self.send_response(200)
+            self.send_header('Accept-Ranges', 'bytes')
+            self.send_header('Content-Length', len(self.value))
+            self.end_headers()
+            self.wfile.write(self.value[0:self.max_len])
+
+        elif byte_range[0] >= len(self.value):
+            self.send_response(416)
+            self.send_header('Accept-Ranges', 'bytes')
+            self.end_headers()
+
+        else:
+            cr = 'bytes %d-%d/%d' % \
+                 (byte_range[0], len(self.value) - 1, len(self.value))
+            self.send_response(206)
+            self.send_header('Accept-Ranges', 'bytes')
+            self.send_header('Content-Length', len(self.value) - byte_range[0])
+            self.send_header('Content-Range', cr)
+            self.end_headers()
+            self.wfile.write(self.value[byte_range[0]:byte_range[1]+1])
 
 
 # guard function to run a test HTTP server on another thread and reap it when
@@ -45,4 +101,19 @@ class TestDownload(unittest.TestCase):
         with _test_http_server(_handler) as server:
             with download.get(server.url('/'), dict(
                     verifier=_verifier, tries=1)) as data:
+                self.assertEqual(value, data.read())
+
+    def test_download_restart(self):
+        value = "Some random string here."
+
+        def _handler(*args):
+            return _DroppingHandler(value, 4, *args)
+
+        def _verifier(filelike):
+            v = filelike.read() == value
+            return v
+
+        with _test_http_server(_handler) as server:
+            with download.get(server.url('/'), dict(
+                    verifier=_verifier, tries=(len(value) / 4 + 1))) as data:
                 self.assertEqual(value, data.read())
