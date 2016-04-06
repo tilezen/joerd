858a2d8e9546d060f921ca6fd56cc88e3073fc42
Matt Amos <zerebubuth@gmail.com>
2016-04-06 20:05:13 +0100

    Added tests for queue / dispatcher job handling, batching and flushing.

--- tests/test_dispatcher.py
+++ tests/test_dispatcher.py
@@ -1,6 +1,10 @@
 import unittest
+import joerd.output.terrarium as terrarium
+from joerd.region import Region
+from joerd.util import BoundingBox
 import joerd.dispatcher as dispatcher
 import sys
+import logging
 
 
 class TestDispatcher(unittest.TestCase):
@@ -26,3 +30,104 @@ class TestDispatcher(unittest.TestCase):
             # check that the thawed value is the same as the original
             value = dispatcher._thaw(k)
             self.assertEqual(example, value)
+
+    def test_dispatch_tiles(self):
+        regions = [
+            Region(BoundingBox(-124.56, 32.4, -114.15, 42.03), [8, 10])
+        ]
+        t = terrarium.Terrarium(regions, [])
+
+        class Batch(object):
+            def __init__(self, queue, max_batch_len):
+                self.queue = queue
+                self.batch = []
+
+            def append(self, job):
+                self.batch.append(job)
+
+            def flush(self):
+                for job in self.batch:
+                    self.queue.append(job)
+                self.batch = []
+
+        class Queue(object):
+            def __init__(self):
+                self.expected = set([
+                    (8, 41, 99),
+                    (8, 43, 98)
+                ])
+
+            def start_batch(self, max_batch_len):
+                return Batch(self, max_batch_len)
+
+            def append(self, tile):
+                coord = (tile.z, tile.x, tile.y)
+                if coord in self.expected:
+                    self.expected.remove(coord)
+
+            def flush(self):
+                pass
+
+        logger = logging.getLogger('process')
+        queue = Queue()
+        d = dispatcher.Dispatcher(queue, 10, logger)
+
+        for tile in t.generate_tiles():
+            d.append(tile)
+
+        d.flush()
+        self.assertEqual(queue.expected, set([]))
+
+    def test_group_dispatch_tiles(self):
+        regions = [
+            Region(BoundingBox(-124.56, 32.4, -114.15, 42.03), [8, 10])
+        ]
+        t = terrarium.Terrarium(regions, [])
+
+        class Batch(object):
+            def __init__(self, queue, max_batch_len):
+                self.queue = queue
+                self.batch = []
+
+            def append(self, job):
+                self.batch.append(job)
+
+            def flush(self):
+                for job in self.batch:
+                    self.queue.append(job)
+                self.batch = []
+
+        class Queue(object):
+            def __init__(self):
+                self.expected = set([
+                    (8, 41, 99),
+                    (8, 43, 98)
+                ])
+
+            def start_batch(self, max_batch_len):
+                return Batch(self, max_batch_len)
+
+            def append(self, tile):
+                coord = (tile['z'], tile['x'], tile['y'])
+                if coord in self.expected:
+                    self.expected.remove(coord)
+
+            def flush(self):
+                pass
+
+        class FakeLogger(object):
+            def info(self, msg):
+                print msg
+
+            def warning(self, msg):
+                print msg
+
+        logger = FakeLogger()
+        queue = Queue()
+        d = dispatcher.GroupingDispatcher(queue, 10, logger, 1000)
+
+        for tile in t.generate_tiles():
+            d.append(tile.freeze_dry())
+
+        d.flush()
+        self.assertEqual(queue.expected, set([]))
--- tests/test_terrarium.py
+++ tests/test_terrarium.py
@@ -1,5 +1,7 @@
 import unittest
 import joerd.output.terrarium as terrarium
+from joerd.region import Region
+from joerd.util import BoundingBox
 
 
 class TestTerrariumTiles(unittest.TestCase):
@@ -17,3 +19,18 @@ class TestTerrariumTiles(unittest.TestCase):
         cx = 0.5 * (ll_bbox.bounds[0] + ll_bbox.bounds[2])
         cy = 0.5 * (ll_bbox.bounds[1] + ll_bbox.bounds[3])
         self.assertEqual((x, y), t.mercator.lonlat_to_xy(z, cx, cy))
+
+    def test_generate_tiles(self):
+        regions = [
+            Region(BoundingBox(-124.56, 32.4, -114.15, 42.03), [8, 10])
+        ]
+        t = terrarium.Terrarium(regions, [])
+        expected = set([
+            (8, 41, 99),
+            (8, 43, 98)
+        ])
+        for tile in t.generate_tiles():
+            coord = (tile.z, tile.x, tile.y)
+            if coord in expected:
+                expected.remove(coord)
+        self.assertEqual(expected, set([]))
