37a7838e127784b16a958dfd8e58d407ebef8d4d
Michal Migurski <mike@teczno.com>
2016-08-22 15:21:46 -0700

    Completed collect script with download function

--- scripts/collect.py
+++ scripts/collect.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from math import log, tan, pi
 from itertools import product
 from argparse import ArgumentParser
 from os.path import join
-import tempfile, shutil
-import unittest
+
+import tempfile, shutil, urllib.request, io, sys, subprocess
+import unittest, unittest.mock as mock
+
+tile_url = 'https://terrain-preview.mapzen.com/geotiff/{z}/{x}/{y}.tif'
 
 def mercator(lat, lon, zoom):
     ''' Convert latitude, longitude to z/x/y tile coordinate at given zoom.
@@ -38,17 +41,75 @@ def tiles(zoom, lat1, lon1, lat2, lon2):
     
     return tiles
 
+def download(output_tif, tiles, verbose=True):
+    ''' Download list of tiles to a temporary directory and return its name.
+    '''
+    dir = tempfile.mkdtemp(prefix='collect-')
+    
+    try:
+        files = []
+
+        for (z, x, y) in tiles:
+            response = urllib.request.urlopen(tile_url.format(z=z, x=x, y=y))
+            if response.status != 200:
+                raise RuntimeError('No such tile: {}'.format((z, x, y)))
+            if verbose:
+                print('Downloaded', response.url, file=sys.stderr)
+
+            with io.open(join(dir, '{}-{}-{}.tif'.format(z, x, y)), 'wb') as file:
+                file.write(response.read())
+                files.append(file.name)
+        
+        if verbose:
+            print('Combining', len(files), 'into', output_tif, '...')
+        temp_tif = join(dir, 'temp.tif')
+        subprocess.check_call(['gdal_merge.py', '-o', temp_tif] + files)
+        shutil.move(temp_tif, output_tif)
+    
+    finally:
+        shutil.rmtree(dir)
+
 class TestCollect (unittest.TestCase):
+
     def test_mercator(self):
         self.assertEqual(mercator(0, 0, 0), (0, 0, 0))
         self.assertEqual(mercator(0, 0, 16), (16, 32768, 32768))
         self.assertEqual(mercator(37.79125, -122.39197, 16), (16, 10487, 25327))
         self.assertEqual(mercator(40.74418, -73.99047, 16), (16, 19298, 24632))
         self.assertEqual(mercator(-35.30816, 149.12446, 16), (16, 59915, 39645))
+
     def test_tiles(self):
         self.assertEqual(tiles(16, 37.79125, -122.39197, 37.79125, -122.39197), [(16, 10487, 25327)])
         self.assertEqual(tiles(16, 0.00034, -0.00056, -0.00034, 0.00043), [(16, 32767, 32767), (16, 32768, 32767), (16, 32767, 32768), (16, 32768, 32768)])
         self.assertEqual(tiles(16, -0.00034, 0.00043, 0.00034, -0.00056), [(16, 32767, 32767), (16, 32768, 32767), (16, 32767, 32768), (16, 32768, 32768)])
+
+    @mock.patch('io.open')
+    @mock.patch('shutil.move')
+    @mock.patch('shutil.rmtree')
+    @mock.patch('tempfile.mkdtemp')
+    @mock.patch('urllib.request.urlopen')
+    @mock.patch('subprocess.check_call')
+    def test_download(self, check_call, urlopen, mkdtemp, rmtree, move, open):
+        mkdtemp.return_value = '/tmp'
+        urlopen.return_value.status = 200
+        open.return_value.__enter__.return_value.name = '/tmp/tile.tif'
+
+        download('/tmp/output.tif', [
+            (12, 656, 1582),
+            (12, 657, 1582),
+            (12, 658, 1582),
+            ], False)
+        
+        rmtree.assert_called_once_with(tempfile.mkdtemp.return_value)
+        
+        self.assertEqual(urlopen.mock_calls[::2], [
+            mock.call('https://terrain-preview.mapzen.com/geotiff/12/656/1582.tif'),
+            mock.call('https://terrain-preview.mapzen.com/geotiff/12/657/1582.tif'),
+            mock.call('https://terrain-preview.mapzen.com/geotiff/12/658/1582.tif')
+            ])
+        
+        check_call.assert_called_once_with(['gdal_merge.py', '-o', '/tmp/temp.tif', '/tmp/tile.tif', '/tmp/tile.tif', '/tmp/tile.tif'])
+        move.assert_called_once_with('/tmp/temp.tif', '/tmp/output.tif')
 
 parser = ArgumentParser(description='Collect Mapzen elevation tiles into a single GeoTIFF')
 
@@ -72,5 +133,4 @@ if __name__ == '__main__':
         result = unittest.TextTestRunner(verbosity=2).run(suite)
         exit(0 if result.wasSuccessful() else 1)
     
-    for (z, x, y) in tiles(args.zoom, *args.bounds):
-        print(z, x, y)
+    download(args.output_tif, tiles(args.zoom, *args.bounds))
