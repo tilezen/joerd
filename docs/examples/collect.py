ba86ad71913cc0b33a38fe783269d8ff3538281f
Nathaniel Kelso <nvkelso@gmail.com>
2017-01-31 15:46:15 -0800

    ? instead of & for url params on mapzen API key, thread the API key thru functions

--- docs/examples/collect.py
+++ docs/examples/collect.py
@@ -8,24 +8,25 @@ from os.path import join, splitext
 import tempfile, shutil, urllib, io, sys, subprocess
 import unittest
 
-tile_url = 'https://tile.mapzen.com/mapzen/terrain/v1/geotiff/{z}/{x}/{y}.tif&api_key={k}'
+# four formats are available, let's use GeoTIFF
+tile_url = 'https://tile.mapzen.com/mapzen/terrain/v1/geotiff/{z}/{x}/{y}.tif?api_key={k}'
 
-#default API key, please set your own
-api_key = 'mapzen-SPsSJJY'
+# default API key, please set your own
+default_api_key = 'mapzen-SPsSJJY'
 
 def mercator(lat, lon, zoom):
     ''' Convert latitude, longitude to z/x/y tile coordinate at given zoom.
     '''
     # convert to radians
     x1, y1 = lon * pi/180, lat * pi/180
-    
+
     # project to mercator
     x2, y2 = x1, log(tan(0.25 * pi + 0.5 * y1))
-    
+
     # transform to tile space
     tiles, diameter = 2 ** zoom, 2 * pi
     x3, y3 = int(tiles * (x2 + pi) / diameter), int(tiles * (pi - y2) / diameter)
-    
+
     return zoom, x3, y3
 
 def tiles(zoom, lat1, lon1, lat2, lon2):
@@ -34,25 +35,25 @@ def tiles(zoom, lat1, lon1, lat2, lon2):
     # convert to geographic bounding box
     minlat, minlon = min(lat1, lat2), min(lon1, lon2)
     maxlat, maxlon = max(lat1, lat2), max(lon1, lon2)
-    
+
     # convert to tile-space bounding box
     _, xmin, ymin = mercator(maxlat, minlon, zoom)
     _, xmax, ymax = mercator(minlat, maxlon, zoom)
-    
+
     # generate a list of tiles
     xs, ys = range(xmin, xmax+1), range(ymin, ymax+1)
     tiles = [(zoom, x, y) for (y, x) in product(ys, xs)]
-    
+
     return tiles
 
-def download(output_path, tiles, verbose=True):
+def download(output_path, tiles, api_key, verbose=True):
     ''' Download list of tiles to a temporary directory and return its name.
     '''
     dir = tempfile.mkdtemp(prefix='collected-')
     _, ext = splitext(output_path)
-    
+
     merge_geotiff = bool(ext.lower() in ('.tif', '.tiff', '.geotiff'))
-    
+
     try:
         files = []
 
@@ -66,7 +67,7 @@ def download(output_path, tiles, verbose=True):
             with io.open(join(dir, '{}-{}-{}.tif'.format(z, x, y)), 'wb') as file:
                 file.write(response.read())
                 files.append(file.name)
-        
+
         if merge_geotiff:
             if verbose:
                 print('Combining', len(files), 'into', output_path, '...', file=sys.stderr)
@@ -77,7 +78,7 @@ def download(output_path, tiles, verbose=True):
             if verbose:
                 print('Moving', dir, 'to', output_path, '...', file=sys.stderr)
             shutil.move(dir, output_path)
-    
+
     finally:
         if merge_geotiff:
             shutil.rmtree(dir)
@@ -113,16 +114,16 @@ class TestCollect (unittest.TestCase):
                 (12, 657, 1582),
                 (12, 658, 1582),
                 ], False)
-        
+
             self.assertEqual(len(rmtree.mock_calls), 0)
             self.assertEqual(len(check_call.mock_calls), 0)
-        
+
             self.assertEqual(urlopen.mock_calls[::3], [
-                mock.call('https://tile.mapzen.com/mapzen/terrain/v1/geotiff/12/656/1582.tif&api_key=mapzen-SPsSJJY'),
-                mock.call('https://tile.mapzen.com/mapzen/terrain/v1/geotiff/12/657/1582.tif&api_key=mapzen-SPsSJJY'),
-                mock.call('https://tile.mapzen.com/mapzen/terrain/v1/geotiff/12/658/1582.tif&api_key=mapzen-SPsSJJY')
+                mock.call('https://tile.mapzen.com/mapzen/terrain/v1/geotiff/12/656/1582.tif?api_key=mapzen-SPsSJJY'),
+                mock.call('https://tile.mapzen.com/mapzen/terrain/v1/geotiff/12/657/1582.tif?api_key=mapzen-SPsSJJY'),
+                mock.call('https://tile.mapzen.com/mapzen/terrain/v1/geotiff/12/658/1582.tif?api_key=mapzen-SPsSJJY')
                 ])
-        
+
             move.assert_called_once_with('/tmp', '/tmp/output')
 
     def test_download_merge(self):
@@ -142,15 +143,15 @@ class TestCollect (unittest.TestCase):
                 (12, 657, 1582),
                 (12, 658, 1582),
                 ], False)
-        
+
             rmtree.assert_called_once_with(tempfile.mkdtemp.return_value)
-        
+
             self.assertEqual(urlopen.mock_calls[::3], [
-                mock.call('https://tile.mapzen.com/mapzen/terrain/v1/geotiff/12/656/1582.tif&api_key=mapzen-SPsSJJY'),
-                mock.call('https://tile.mapzen.com/mapzen/terrain/v1/geotiff/12/657/1582.tif&api_key=mapzen-SPsSJJY'),
-                mock.call('https://tile.mapzen.com/mapzen/terrain/v1/geotiff/12/658/1582.tif&api_key=mapzen-SPsSJJY')
+                mock.call('https://tile.mapzen.com/mapzen/terrain/v1/geotiff/12/656/1582.tif?api_key=mapzen-SPsSJJY'),
+                mock.call('https://tile.mapzen.com/mapzen/terrain/v1/geotiff/12/657/1582.tif?api_key=mapzen-SPsSJJY'),
+                mock.call('https://tile.mapzen.com/mapzen/terrain/v1/geotiff/12/658/1582.tif?api_key=mapzen-SPsSJJY')
                 ])
-        
+
             check_call.assert_called_once_with(['gdal_merge.py', '-o', '/tmp/temp.tif', '/tmp/tile.tif', '/tmp/tile.tif', '/tmp/tile.tif'])
             move.assert_called_once_with('/tmp/temp.tif', '/tmp/output.tif')
 
@@ -169,18 +170,18 @@ parser.add_argument('--bounds', metavar='DEG', type=float, nargs=4,
 parser.add_argument('--zoom', type=int, default=12,
                     help='Map zoom level given as integer. Defaults to 12.')
 
-parser.add_argument('--api-key', type=str, default=None,
+parser.add_argument('--mapzen_api_key', default=default_api_key,
                     help='Mapzen API key required to use the terrain tile service. See: https://mapzen.com/documentation/overview/#developer-accounts-and-api-keys.')
 
 parser.add_argument('output_path', help='Output GeoTIFF filename or local directory name.')
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    
+
     if args.testing:
         import mock # To run tests, `pip install mock==2.0.0`
         suite = unittest.defaultTestLoader.loadTestsFromName(__name__)
         result = unittest.TextTestRunner(verbosity=2).run(suite)
         exit(0 if result.wasSuccessful() else 1)
-    
-    download(args.output_path, tiles(args.zoom, *args.bounds))
+
+    download(args.output_path, tiles(args.zoom, *args.bounds), args.mapzen_api_key)
