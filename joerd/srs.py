329f3601efd6ec92b1102407defad86ef818c528
David Rupnik <davidrupnik23@gmail.com>
2020-10-29 00:03:53 +0100

    Intial release. Note, to test it, you need to manually create the index and source files for srtm, as the srtm script fails. You also need to install PDAL with either LASzip or LAZperf

--- joerd/srs.py
+++ joerd/srs.py
@@ -12,6 +12,15 @@ NAD83_WKT = 'GEOGCS["NAD83",DATUM["North_American_Datum_1983",SPHEROID[' \
             'AUTHORITY["EPSG","6269"]],PRIMEM["Greenwich",0],UNIT["degree",' \
             '0.0174532925199433],AUTHORITY["EPSG","4269"]]'
 
+D96_WKT = 'PROJCS["Slovenia 1996 / Slovene National Grid",GEOGCS["Slovenia 1996",' \
+'DATUM["Slovenia_Geodetic_Datum_1996",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],' \
+'TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6765"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],' \
+'UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4765"]],PROJECTION["Transverse_Mercator"],' \
+'PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",15],PARAMETER["scale_factor",0.9999],' \
+'PARAMETER["false_easting",500000],PARAMETER["false_northing",-5000000],UNIT["metre",1,AUTHORITY["EPSG","9001"]],' \
+'AXIS["Easting",EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","3794"]]'
+
+
 
 def wgs84():
     sr = osr.SpatialReference()
@@ -22,4 +31,9 @@ def wgs84():
 def nad83():
     sr = osr.SpatialReference()
     sr.ImportFromWkt(NAD83_WKT)
+    return sr
+
+def d96():
+    sr = osr.SpatialReference()
+    sr.ImportFromWkt(D96_WKT)
     return sr
